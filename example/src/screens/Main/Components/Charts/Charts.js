47ff023009d916954ebad73f408df58608c0ab6f
Anh Duc <anhduc5997@gmail.com>
2018-11-15 16:41:10 +0700

    charts

--- example/src/screens/Main/Components/Charts/Charts.js
+++ example/src/screens/Main/Components/Charts/Charts.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import { Text, View, ScrollView } from "react-native";
 
 import AreaCharts from "./AreaCharts";
-import BarCharts from "./BarCharts";
-import LineCharts from "./LineCharts";
-import PieCharts from "./PieCharts";
-import StackedBarCharts from "./StackedBarCharts ";
 
 import styles from "./styles";
 
@@ -16,13 +12,6 @@ export default class Charts extends Component {
         <View style={styles.wrapper}>
           <AreaCharts />
 
-          <BarCharts />
-
-          <LineCharts />
-
-          <PieCharts />
-
-          <StackedBarCharts />
         </View>
       </ScrollView>
     );
